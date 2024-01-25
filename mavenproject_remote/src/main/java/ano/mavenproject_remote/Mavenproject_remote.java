0f93c8fd060cd4ea360b8ae14accbd9d270bde4f
Fernando Rocas <>
2024-01-25 11:39:32 +0000

    mensagem de commit

---
+++ mavenproject_remote/src/main/java/ano/mavenproject_remote/Mavenproject_remote.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+//exemplo para remoto
+package ano.mavenproject_remote;
+
+/**
+ *
+ * @author ferna
+ */
+public class Mavenproject_remote {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
